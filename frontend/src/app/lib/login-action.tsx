0f2c0caa822bdeb7883ec8c46e201f86baddc653
ankush kumar <ankush@gmail.com>
2024-04-06 21:03:42 +0530

    Alway typo is mistake

--- frontend/src/app/lib/login-action.tsx
+++ frontend/src/app/lib/login-action.tsx
@@ -4,25 +4,27 @@ export async function loginAction(prevState:any,formData: any) {
     const url = `${STRAPI_URL}/api/auth/local`;
     console.log(url , "s")
     const validatedFields = {
-        email: formData.get("email"),
+        identifier: formData.get("identifier"),
         password: formData.get("password"),
       }
     console.log(validatedFields)
-    const {email , password} = validatedFields
-    console.log(email)
+    const {identifier , password} = validatedFields
+    console.log(identifier)
     try {
       const response  = await fetch(url, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application.json'
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({user:{email, password}}),
+        body: JSON.stringify({identifier, password}),
         cache: "no-cache",
       })
-
-      console.log(response)
+      if (!response.ok) throw  new Error('HTTP error! status: ${response.status}');
+      const respons = await response.json();
+      console.log(respons)
       
     } catch (error) {
+      console.log(error);
       throw error
     }
     return 1
